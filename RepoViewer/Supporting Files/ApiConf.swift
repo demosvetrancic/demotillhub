7f7ca68114c975ff4091a25d563814153e2252f9
Svetozar Rancic <svetrancic@svetozars-macbook-pro.local>
2017-10-10 21:43:40 +0200

    project created apiconf added

---
+++ RepoViewer/Supporting Files/ApiConf.swift
@@ -0,0 +1,14 @@
+//
+//  ApiConf.swift
+//  RepoViewer
+//
+//  Created by Svetozar Rancic on 10/10/17.
+//  Copyright © 2017 svetrancic. All rights reserved.
+//
+
+import Foundation
+
+struct GitApi
+{
+    static let baseUrl = "https://api.github.com/search/repositories?q=tetris"
+}
