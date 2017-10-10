f5d5809e73b9645269943360a2a06d19de7385f1
Svetozar Rancic <svetrancic@svetozars-macbook-pro.local>
2017-10-11 00:06:34 +0200

    edited RepoCollectionViewController 	added properties: pages, count, searchterm 	redefined methods for feching, updatingDatabase with some security prints

--- RepoViewer/Controllers/RepoCollectionViewController.swift
+++ RepoViewer/Controllers/RepoCollectionViewController.swift
@@ -7,25 +7,109 @@
 //
 
 import UIKit
+import CoreData
 
-class RepoCollectionViewController : UIViewController {
 
+/// class for repo collection view scene
+class RepoCollectionViewController : UIViewController{
+    
+    // MARK: Properties
+    
+    /// presistent container for the scheme model RepoViewer.xcdatamodel
+    var container: NSPersistentContainer? = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    
+    /// currentPage that is requested
+    var currentPage = 1
+    
+    /// totalNumber pages - gets by total_count/GitApi.defaultPerPage
+    var totalPages = 1
+    
+    /// current search term for query to git.
+    var searchTerm: String?
+        {
+        /// placing closure on didSet to restart paging and fetch for new term, whenever user sets a new search term
+        didSet
+        {
+            currentPage = 1
+            totalPages = 1
+            fetchRepos(withTerm: searchTerm, withPage: currentPage)
+        }
+    }
+    
+    //MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //
-        BaseService.shared.getRepos(withParam: "tetris",withPage: 1, withCompletion:
+       
+        
+        self.searchTerm = GitApi.defaultParam
+        
+    }
+    
+    private func fetchRepos(withTerm term:String?, withPage page: Int)
+    {
+        BaseService.shared.getRepos(withParam: term, withPage: page, withCompletion: {
+            (items, totalCount) in
+            if (items != nil)
             {
-                items in
-                print("return: \(items)")
+                self.totalPages = totalCount/GitApi.defaultPerPage
+                self.updateDatabase(with: items!)
+                //self?.searchTerm = "tetris"
+            }
+     
         })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    private func updateDatabase(with repos: [[String: Any]])
+    {
+        print("loading database...")
+        container?.performBackgroundTask
+            {
+                [weak self] context in
+                
+                for repoInfo in repos {
+                    _ = try? Repository.findOrCreateRepo(matching: repoInfo as NSDictionary, in: context)
+                }
+                try? context.save()
+                print("done loading database...")
+                self?.printDatabaseStatistics()
+        }
     }
+    
+    func printDatabaseStatistics()
+    {
+        if let context = container?.viewContext {
+            // context on main
+            context.perform {
+                // for security
+                if Thread.isMainThread {
+                    print("main")
+                } else {
+                    print("off main")
+                }
+                
+                // checking count not to duplicate same objects
+                if let repoCount = try? context.count(for: Repository.fetchRequest())
+                {
+                    print("Number of repositories: \(repoCount)")
+                    
+                }
+                //todo self.updateOutlets()
+                
+            }
+        }
+    }
+    
+
+    
+    
+    
 
 
+    
 }
+
+
 
